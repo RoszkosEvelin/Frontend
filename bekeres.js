45f79891d68df5389e2ff7c477cdbb0174bfe7b2
Roszkos Evelin <sz4roseve@vasvari.org>
2025-09-10 08:39:19 +0200

    Add files via upload

---
+++ bekeres.js
@@ -0,0 +1,9 @@
+function kuldes(){
+    let nev=document.getElementById('nev').value
+    let iszam=document.getElementById('iranyitoszam').value
+    let varos=document.getElementById('varos').value
+    let utca=document.getElementById('utca').value
+    let hazszam=document.getElementById('hazszam').value
+
+    console.log(nev,iszam,varos,utca,hazszam)
+}
